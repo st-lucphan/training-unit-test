21cf8dc4b5a48a48238df99684df5631a8fc1e79
st-lucphan <luc.phan@supremetech.vn>
2023-04-17 09:25:12 +0700

    Update mock localStorage

--- src/app/core/services/authStorage.service.test.ts
+++ src/app/core/services/authStorage.service.test.ts
@@ -1,63 +1,24 @@
 import { AuthStorageService } from "./authStorage.service";
-
-const localStorageMock = (function () {
-  let store = {};
-
-  return {
-    getItem(key) {
-      return store[key];
-    },
-
-    setItem(key, value) {
-      store[key] = value;
-    },
-
-    clear() {
-      store = {};
-    },
-
-    removeItem(key) {
-      delete store[key];
-    },
-
-    getAll() {
-      return store;
-    },
-  };
-})();
-
-Object.defineProperty(window, "localStorage", {
-  value: localStorageMock,
-  writable: true,
-});
-const setItemMock = jest.fn();
-const getItemMock = jest.fn();
-const removeItemMock = jest.fn();
 describe("authStorage service", () => {
-  beforeEach(() => {
-    globalThis.window.localStorage.setItem = setItemMock;
-    globalThis.window.localStorage.getItem = getItemMock;
-    globalThis.window.localStorage.removeItem = removeItemMock;
-  });
-
-  afterEach(() => {
-    setItemMock.mockClear();
-    getItemMock.mockClear();
-    removeItemMock.mockClear();
-  });
   const authStorageService = new AuthStorageService();
   it("getItem method should be called once", () => {
+    const getItemMock = jest.spyOn(Storage.prototype, "getItem");
     authStorageService.getToken();
     expect(getItemMock).toBeCalled();
+    expect(getItemMock).toBeCalledWith("token");
   });
+
   it("setItem method should be called with the correct parameter", () => {
+    const setItemMock = jest.spyOn(Storage.prototype, "setItem");
     authStorageService.setToken("test");
     expect(setItemMock).toBeCalled();
     expect(setItemMock).toBeCalledWith("token", "test");
   });
 
   it("removeItem method should be called once", () => {
+    const removeItemMock = jest.spyOn(Storage.prototype, "removeItem");
     authStorageService.removeToken();
     expect(removeItemMock).toBeCalled();
+    expect(removeItemMock).toBeCalledWith("token");
   });
 });
