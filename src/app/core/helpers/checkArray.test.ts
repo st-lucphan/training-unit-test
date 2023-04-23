3babab3c8d096c15dd504018d0674b0b51407fce
st-lucphan <luc.phan@supremetech.vn>
2023-04-23 21:10:05 +0700

    Format code

--- src/app/core/helpers/checkArray.test.ts
+++ src/app/core/helpers/checkArray.test.ts
@@ -1,29 +1,29 @@
-import { isAscendingArray } from "./checkArray";
+import { isAscendingArray } from './checkArray';
 
-describe("ascending array function", () => {
-  describe("empty array or undefined parameters", () => {
-    it("should return false", () => {
+describe('ascending array function', () => {
+  describe('empty array or undefined parameters', () => {
+    it('should return false', () => {
       expect(isAscendingArray([])).toBeFalsy();
       expect(isAscendingArray(null)).toBeFalsy();
       expect(isAscendingArray(undefined)).toBeFalsy();
     });
   });
-  describe("array with 1 element", () => {
-    it("should return false", () => {
+  describe('array with 1 element', () => {
+    it('should return false', () => {
       expect(isAscendingArray([null])).toBeFalsy();
       expect(isAscendingArray([undefined])).toBeFalsy();
     });
-    it("should return true", () => {
+    it('should return true', () => {
       expect(isAscendingArray([1])).toBeTruthy();
     });
   });
-  describe("array with many elements", () => {
-    it("should return false", () => {
+  describe('array with many elements', () => {
+    it('should return false', () => {
       expect(isAscendingArray([1, 7, 5])).toBeFalsy();
       expect(isAscendingArray([1, 5, 7, null])).toBeFalsy();
       expect(isAscendingArray([1, -5, 7])).toBeFalsy();
     });
-    it("should return true", () => {
+    it('should return true', () => {
       expect(isAscendingArray([1, 2, 4])).toBeTruthy();
       expect(isAscendingArray([1.23, 1.234, 4])).toBeTruthy();
       expect(isAscendingArray([0, 0, 4])).toBeTruthy();
