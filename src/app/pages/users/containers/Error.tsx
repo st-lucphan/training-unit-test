fb60172569bd76d182ac75b70c2e6e567b6df1c5
st-lucphan <luc.phan@supremetech.vn>
2023-04-23 20:52:31 +0700

    format code

--- src/app/pages/users/containers/Error.tsx
+++ src/app/pages/users/containers/Error.tsx
@@ -1,15 +1,6 @@
-import React, { useEffect, useState } from "react";
-import { deleteUser, getListUser, getUserInfo } from "../user.action";
-import { useDispatch, useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
+import React from 'react';
 
 const Error = () => {
-  const { id } = useParams();
-  const dispatch = useDispatch();
-  const userData = useSelector((state: any) => state.userReducer.dataUser);
-  const { isLoading } = useSelector((state: any) => state.userReducer);
-  const [isLoadingIndicator, setIsLoadingIndicator] = useState(true);
-
   return (
     <>
       <p>Error</p>
