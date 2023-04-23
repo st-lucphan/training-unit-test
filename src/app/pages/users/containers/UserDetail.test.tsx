a30669e887bc1870a8a1e151814a8fa9df1328c8
st-lucphan <luc.phan@supremetech.vn>
2023-04-23 17:28:47 +0700

    Apply unit test for UserDetail component

--- src/app/pages/users/containers/UserDetail.test.tsx
+++ src/app/pages/users/containers/UserDetail.test.tsx
@@ -1,62 +1,45 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
-import { fireEvent, render, screen } from "@testing-library/react";
-import UserList from "./UserList";
+import { render, screen } from "@testing-library/react";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
 import { MOCK_INITIAL_STATE } from "../../../../test/mocks/redux";
+import UserDetail from "./UserDetail";
 
 const mockUseNavigate = jest.fn();
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
   useNavigate: () => mockUseNavigate,
 }));
-describe("UserList", () => {
-  describe("UserList loading", () => {
+
+describe("UserDetail", () => {
+  describe("UserDetail loading", () => {
     const mockStore = configureStore();
     const store = mockStore(MOCK_INITIAL_STATE.loading);
     it("should render correctly", () => {
       render(
         <Provider store={store}>
           <BrowserRouter>
-            <UserList />
+            <UserDetail />
           </BrowserRouter>
         </Provider>
       );
       expect(screen.getByText("Loading")).toBeInTheDocument;
     });
   });
-  describe("UserList loaded", () => {
+  describe("UserDetail loaded", () => {
     const mockStore = configureStore();
     const store = mockStore(MOCK_INITIAL_STATE.loaded);
     it("should render correctly", () => {
       render(
         <Provider store={store}>
           <BrowserRouter>
-            <UserList />
+            <UserDetail />
           </BrowserRouter>
         </Provider>
       );
-      expect(screen.getByText("User List")).toBeInTheDocument;
+      expect(screen.getByText("User Information")).toBeInTheDocument;
       expect(screen.getByText("Leanne Graham")).toBeInTheDocument;
-      expect(screen.getAllByTestId("user-item").length).toBe(2);
-    });
-  });
-
-  describe("Delete User", () => {
-    const mockStore = configureStore();
-    const store = mockStore(MOCK_INITIAL_STATE.loaded);
-    it("should delete successfully", () => {
-      render(
-        <Provider store={store}>
-          <BrowserRouter>
-            <UserList />
-          </BrowserRouter>
-        </Provider>
-      );
-      const btnDelete = screen.getAllByText("Delete");
-      fireEvent.click(btnDelete[0]);
-      expect(screen.getAllByTestId("user-item").length).toBe(1);
     });
   });
 
@@ -67,7 +50,7 @@ describe("UserList", () => {
       render(
         <Provider store={store}>
           <BrowserRouter>
-            <UserList />
+            <UserDetail />
           </BrowserRouter>
         </Provider>
       );
--- src/app/pages/users/containers/UserDetail.tsx
+++ src/app/pages/users/containers/UserDetail.tsx
@@ -1,23 +1,24 @@
-import React, { useEffect, useState } from "react";
-import { deleteUser, getListUser, getUserInfo } from "../user.action";
+import React, { useEffect } from "react";
+import { getUserInfo } from "../user.action";
 import { useDispatch, useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const UserList = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const dispatch = useDispatch();
-  const userData = useSelector((state: any) => state.userReducer.dataUser);
+  const { dataUser, hasError } = useSelector((state: any) => state.userReducer);
   const { isLoading } = useSelector((state: any) => state.userReducer);
-  const [isLoadingIndicator, setIsLoadingIndicator] = useState(true);
 
   useEffect(() => {
     dispatch(getUserInfo(id));
-    // if (!isLoading) {
-    //   setTimeout(() => {
-    //     setIsLoadingIndicator(false);
-    //   }, 1000);
-    // }
   }, []);
+
+  useEffect(() => {
+    if (hasError) {
+      navigate("/user/error");
+    }
+  }, [hasError]);
 
   return (
     <>
@@ -25,28 +26,28 @@ const UserList = () => {
         <p>Loading</p>
       ) : (
         <>
-          {userData && (
+          {dataUser && (
             <div className="container">
               <div className="card-body p-4">
-                <h6 className="txt-bold">Information</h6>
+                <h6 className="txt-bold">User Information</h6>
                 <div className="row justify-content-center pt-1 ">
                   <div className="col-6 mb-3">
                     <h6>Name</h6>
-                    <p className="text-muted">{userData.name}</p>
+                    <p className="text-muted">{dataUser.name}</p>
                   </div>
                   <div className="col-6 mb-3">
                     <h6>Phone</h6>
-                    <p className="text-muted">{userData.phone}</p>
+                    <p className="text-muted">{dataUser.phone}</p>
                   </div>
                 </div>
                 <div className="row justify-content-center pt-1">
                   <div className="col-6 mb-3">
                     <h6>Email</h6>
-                    <p className="text-muted">{userData.email}</p>
+                    <p className="text-muted">{dataUser.email}</p>
                   </div>
                   <div className="col-6 mb-3">
                     <h6>Website</h6>
-                    <p className="text-muted">{userData.website}</p>
+                    <p className="text-muted">{dataUser.website}</p>
                   </div>
                 </div>
               </div>
